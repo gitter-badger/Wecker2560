41668398eea4bd32b53e1dd5a30012b6d4ae3976
Leonardo I <mondloslp@gmail.com>
2019-05-16 16:28:54 +0200

    init

---
+++ Wecker/Wecker.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal.h>
+#include "read.h"
+
+enum{rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7};
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  analogWrite(10, 0);
+  lcd.print("hey");
+
+}
+
+void loop() {
+  lcd_key = read_LCD_buttons();
+  if(buttonPressed(lcd_key)){
+    long long int t = millis();
+    analogWrite(10, 150);
+    while(millis()-t < 2000);
+    analogWrite(10, 0);  
+  }
+}
